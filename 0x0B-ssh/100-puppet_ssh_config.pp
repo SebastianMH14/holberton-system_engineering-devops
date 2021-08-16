51b09b14a9149de76d7a91d8043d284587e8ef80
SebastianMH14 <sebasmh2002@hotmail.com>
2021-08-16 12:55:27 -0500

    add task advanced

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#chnague config file
+
+file_line { 'use private key':
+    path    => '/etc/ssh/ssh_config',
+    line    => '    IdentityFile ~/.ssh/holberton'
+    replace => true,
+}
+
+file_line { 'using password':
+    path    => '/etc/ssh/ssh_config',
+    line    => '    PasswordAuthentication no',
+    replace => true,
+}
