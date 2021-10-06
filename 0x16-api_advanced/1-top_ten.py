f3f1dfd246bb98e96e14ad8d4d98ffc355d7c8e1
SebastianMH14 <sebasmh2002@hotmail.com>
2021-10-06 14:33:10 -0500

    [add] task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""queries the Reddit API and prints
+the titles of the first 10 hot posts"""
+import requests
+
+
+def top_ten(subreddit):
+    """the hottest reddits"""
+    params = {"limit": 10}
+    data = requests.get("https://www.reddit.com/r/{}/hot.json".format(
+        subreddit),
+        allow_redirects=False,
+        params=params)
+    if data.status_code == 200:
+        data = data.json()
+        posts = data["data"]["children"]
+        if data["data"]["dist"] == 0:
+            print(None)
+        else:
+            for post in posts:
+                print(post["data"]["title"])
+    else:
+        print(None)
