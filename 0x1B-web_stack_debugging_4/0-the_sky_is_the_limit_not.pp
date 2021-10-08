b3ca6cce39efa07c9b79f9d3f525cf4e66c8a8be
SebastianMH14 <sebasmh2002@hotmail.com>
2021-10-08 12:44:12 -0500

    [add] task 0 and readme

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,14 @@
+# Fix nginx
+
+service { 'nginx stop':
+    ensure => stopped;
+}
+
+exec { 'fix--for-nginx':
+    command => "sed -i 's/15/2000/g' /etc/default/nginx",
+    path    => ['/bin'],
+}
+
+service { 'nginx':
+    ensure => running,
+}
